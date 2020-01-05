28f9d38b3e4ac5a990d39856a712dae59756e4de
Wang Zuo <wzuoadjusted@gmail.com>
2020-01-05 22:23:10 +0800

    webhook test

--- static-node-build/packs/index.js
+++ static-node-build/packs/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Kit } from '@swiftcarrot/kit';
 
-render(<Kit>kit 2<Kit>, document.getElementById('root'));
+render(<Kit>kit<Kit>, document.getElementById('root'));
