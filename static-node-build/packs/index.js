7310bb0f6bac0b8a0f2fe324506ac55e7bd0e818
Wang Zuo <wzuoadjusted@gmail.com>
2020-01-05 19:55:29 +0800

    update page

--- static-node-build/packs/index.js
+++ static-node-build/packs/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Kit } from '@swiftcarrot/kit';
 
-render(<Kit>example<Kit>, document.getElementById('root'));
+render(<Kit>kit<Kit>, document.getElementById('root'));
